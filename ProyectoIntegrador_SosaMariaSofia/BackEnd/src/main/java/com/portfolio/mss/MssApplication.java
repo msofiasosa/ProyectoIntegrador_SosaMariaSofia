4d4df1e489ed92675032f0d3cf7048da74e0c9b2
msofiasosa <sosamariasofia.21@gmail.com>
2023-05-01 19:58:16 -0300

    spring

--- ProyectoIntegrador_SosaMariaSofia/BackEnd/src/main/java/com/portfolio/mss/MssApplication.java
+++ ProyectoIntegrador_SosaMariaSofia/BackEnd/src/main/java/com/portfolio/mss/MssApplication.java
@@ -1,4 +1,4 @@
-package com.portfoliomss.mss;
+package com.portfolio.mss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- ProyectoIntegrador_SosaMariaSofia/BackEnd/src/test/java/com/portfolio/mss/MssApplicationTests.java
+++ ProyectoIntegrador_SosaMariaSofia/BackEnd/src/test/java/com/portfolio/mss/MssApplicationTests.java
@@ -1,4 +1,4 @@
-package com.portfoliomss.mss;
+package com.portfolio.mss;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
